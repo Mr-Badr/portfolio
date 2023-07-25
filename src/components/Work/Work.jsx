54a243bd93868e18d1d16f8d9809b053445890f2
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-07-25 03:10:26 +0100

    Updates

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -40,6 +40,60 @@ const GithubIcon = () => {
     </svg>
   );
 };
+const GithubIconFront = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-github"
+    >
+      <title>Frontend</title>
+      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+    </svg>
+  );
+};
+const GithubIconBack = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-github"
+    >
+      <title>Backend</title>
+      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+    </svg>
+  );
+};
+const GithubIconAdmin = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-github"
+    >
+      <title>Admin panel</title>
+      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+    </svg>
+  );
+};
 
 const VideoIcon = () => {
   return (
@@ -68,6 +122,81 @@ const Work = () => {
         <ul className="inner">
           <li className="work-item" data-aos="fade-up" data-aos-duration="700">
             <div className="project_content">
+              <div className="pt-4">
+                <p className="project_overline">Featured Project</p>
+                <h3 className="project_title">
+                  <a href="" target="_blank">
+                    MERN E-Commerce App
+                  </a>
+                </h3>
+                <div className="project_desc">
+                  <p>
+                    Full Stack e-commerce app created using Node.js with a REST
+                    API and MongoDB in the Backend. HTML, CSS, React.js,
+                    Bootstrap, and Material UI in Both Frontend and Admin Panel.
+                    <br />
+                    <u>
+                      <b>User</b>
+                    </u>{" "}
+                    can create an account, browse all products, and add many
+                    products to the cart, add products to wishlist, compare two
+                    products, read blogs and give feedback ...
+                    <br />
+                    <u>
+                      <b>Admin</b>
+                    </u>{" "}
+                    can manage the products, users, coupons and manage blogs. He
+                    can also manage the orders in my application.
+                  </p>
+                </div>
+                <ul className="project_techs">
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>Express</li>
+                  <li>Node.js</li>
+                  <li>MongoDB</li>
+                  <li>Material UI</li>
+                  <li>Bootstrap</li>
+                </ul>
+                <div className="project_links">
+                  <a
+                    href="https://github.com/Mr-Badr/mern-ecommerce-frontend"
+                    target="_blank"
+                  >
+                    <GithubIconFront />
+                  </a>
+                  <a
+                    href="https://github.com/Mr-Badr/mern-ecommerce-backend"
+                    target="_blank"
+                  >
+                    <GithubIconBack />
+                  </a>
+                  <a
+                    href="https://github.com/Mr-Badr/mern-ecommerce-admin"
+                    target="_blank"
+                  >
+                    <GithubIconAdmin />
+                  </a>
+                  <a
+                    href="https://photos.google.com/share/AF1QipMEnPo7hvTJc__EgkMlOhVNhr_BvslJO9l_u_TWEcl0L3f1nWZkVsA6dLYwPvlw4Q/photo/AF1QipM2w0LP_eEIkWLe3iSm6eU-Dmg2QZO-tgFbG8ql?key=by1FemFpVFg5cmt0bHpELWczS2xuSjFkeEUxZF9R"
+                    target="_blank"
+                  >
+                    <VideoIcon />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="project_image">
+              <a href="" target="_blank">
+                <div className="img">
+                  <img src="/images/mern.png" alt="" />
+                </div>
+              </a>
+            </div>
+          </li>
+
+          <li className="work-item" data-aos="fade-up" data-aos-duration="700">
+            <div className="project_content">
               <div>
                 <p className="project_overline">Featured Project</p>
                 <h3 className="project_title">
@@ -77,10 +206,10 @@ const Work = () => {
                 </h3>
                 <div className="project_desc">
                   <p>
-                    Full Stack Amazon Clone which created using ReactJS with
-                    full E-Commerce Functionality. This is a small web app that
-                    tries to imitate the desktop web version of amazon site, you
-                    can add items to the basket, delete them, and have your user
+                    Full Stack Amazon Clone created using ReactJS with full
+                    E-Commerce Functionality. This is a small web app that tries
+                    to imitate the desktop web version of amazon site, you can
+                    add items to the basket, delete them, and have your user
                     authentification feature thanks to Firebase.
                   </p>
                 </div>
@@ -93,7 +222,7 @@ const Work = () => {
                 </ul>
                 <div className="project_links">
                   <a
-                    href="https://www.linkedin.com/posts/badr-eddine-el-harchali_mernstackdeveloper-webdevelopment-reactjs-activity-6938508950304657409-IreL?utm_source=share&utm_medium=member_desktop"
+                    href="https://github.com/Mr-Badr/Full-Stack-Amazon-Clone"
                     target="_blank"
                   >
                     <GithubIcon />
@@ -108,6 +237,52 @@ const Work = () => {
               <a href="" target="_blank">
                 <div className="img">
                   <img src="/images/work1.png" alt="" />
+                </div>
+              </a>
+            </div>
+          </li>
+
+          <li className="work-item" data-aos="fade-up" data-aos-duration="700">
+            <div className="project_content">
+              <div>
+                <p className="project_overline">Featured Project</p>
+                <h3 className="project_title">
+                  <a href="" target="_blank">
+                    Tic-tac-toe Game
+                  </a>
+                </h3>
+                <div className="project_desc">
+                  <p>
+                    Player-1 starts playing the game and both the players make
+                    their moves in consecutive turns. The player who makes a
+                    straight 3-block chain wins the game.
+                  </p>
+                </div>
+                <ul className="project_techs">
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>Javascript</li>
+                </ul>
+                <div className="project_links">
+                  <a
+                    href="https://github.com/Mr-Badr/Tic-tac-toe"
+                    target="_blank"
+                  >
+                    <GithubIcon />
+                  </a>
+                  <a
+                    href="https://mr-badr.github.io/Tic-tac-toe/"
+                    target="_blank"
+                  >
+                    <ExternalIcon />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="project_image">
+              <a href="" target="_blank">
+                <div className="img">
+                  <img src="/images/xo.png" alt="" />
                 </div>
               </a>
             </div>
@@ -193,7 +368,10 @@ const Work = () => {
                   >
                     <GithubIcon />
                   </a>
-                  <a href="https://mr-badr.github.io/bank_modern_app/" target="_blank">
+                  <a
+                    href="https://mr-badr.github.io/bank_modern_app/"
+                    target="_blank"
+                  >
                     <ExternalIcon />
                   </a>
                 </div>
--- src/data.js
+++ src/data.js
@@ -50,15 +50,6 @@ export const jobs = [
 export const projects = [
   {
     id: 1,
-    title: "Tic-tac-toe Game",
-    description:
-      "Player-1 starts playing the game and both the players make their moves in consecutive turns. The player who makes a straight 3-block chain wins the game.",
-    tech: ["HTML", "CSS", "JavaScript"],
-    github: "https://github.com/Mr-Badr/Tic-tac-toe",
-    live: "https://mr-badr.github.io/Tic-tac-toe/",
-  },
-  {
-    id: 2,
     title: "Random Quote Generator",
     description:
       "this is a ReactJS API data fetching project. We have React on the front end and we'll make get requests to Advice Slip JSON API.",
@@ -67,7 +58,7 @@ export const projects = [
     live: "https://great-random-quotes.netlify.app",
   },
   {
-    id: 3,
+    id: 2,
     title: "Weather application",
     description:
       "React JS weather app using the OpenWeatherMap API. We will be using Axios to connect with our API.",
@@ -76,7 +67,7 @@ export const projects = [
     live: "https://weather-web-appp.netlify.app",
   },
   {
-    id: 4,
+    id: 3,
     title: "To Do List",
     description:
       "JavaScript Todo List helps you create a list of things you want to do throughout the day",
@@ -85,7 +76,7 @@ export const projects = [
     live: "https://mr-badr.github.io/vanilla-todo-list/",
   },
   {
-    id: 5,
+    id: 4,
     title: "Google Maps Clone",
     description: "A simple Google Maps Clone built with Vanilla JavaScript.",
     tech: ["HTML", "CSS", "JavaScript"],
@@ -93,12 +84,20 @@ export const projects = [
     live: "https://mr-badr.github.io/Google-Maps-Clone/",
   },
   {
-    id: 6,
+    id: 5,
     title: "Simple Responsive Website",
-    description:
-      "This is a Responsive Website built only with HTML and CSS.",
+    description: "This is a Responsive Website built only with HTML and CSS.",
     tech: ["HTML", "CSS", "Media Queries"],
     github: "https://github.com/Mr-Badr/Travel-website",
     live: "https://mr-badr.github.io/Travel-website/",
+  },
+  {
+    id: 6,
+    title: "Student Grading System",
+    description:
+      "In this Grading System In Java has Graphical User Interface (GUI) Design, and this system is a user friendly that anyone can manage in easy way.",
+    tech: ["Java"],
+    github: "https://github.com/Mr-Badr/Student-score",
+    live: "https://photos.google.com/share/AF1QipNkN2sk9tJ4jrVFlx7vQOrEmYSDFlUss1KSxoIvVcG_ux1PIpUaKeqiNm0DIIC0gQ/photo/AF1QipNgrUpf54esA-wcK4od64J_4BxDhDlKeNogt3Rt?key=bjNvZFNpUlZ3SXZ2MERKNnFQMkwxUDJLbVhFM0t3",
   },
 ];
