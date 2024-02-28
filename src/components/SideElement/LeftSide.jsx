8f869ec3bf17a37bca43757bc16d57c5c85c5879
Badr Eddine El-Harchali <Mr.Elharchali@gmail.com>
2024-02-28 00:59:27 -0800

    Update LeftSide.jsx

--- src/components/SideElement/LeftSide.jsx
+++ src/components/SideElement/LeftSide.jsx
@@ -6,7 +6,7 @@ const LeftSide = () => {
     <div className="left_side_element" data-aos="fade-up" data-aos-delay="1400">
       <ul>
         <li>
-          <a href="https://github.com/Mr-Badr" target="_blank">
+          <a href="https://gitlab.com/MrBadr" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               role="img"
@@ -18,7 +18,7 @@ const LeftSide = () => {
               strokeLinejoin="round"
               className="feather feather-github"
             >
-              <title>GitHub</title>
+              <title>GitLab</title>
               <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
             </svg>
           </a>
@@ -58,27 +58,6 @@ const LeftSide = () => {
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
               <rect x="2" y="9" width="4" height="12"></rect>
               <circle cx="4" cy="4" r="2"></circle>
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.facebook.com/profile.php?id=100004375517845"
-            target="_blank"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <title>Facebook</title>
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
             </svg>
           </a>
         </li>
